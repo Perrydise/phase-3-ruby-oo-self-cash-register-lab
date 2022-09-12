6f73cf9d2d02c4247f408e4d5609e7868c4d7926
Perry Jorss <pjorss123@gmail.com>
2022-09-11 21:39:41 -0400

    commit tree 1967c0a6db0dd957ac49e2f4a5f39c27080ab436

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total
+    attr_accessor :food_array
+    attr_accessor :last_price
+    attr_accessor :last_quanity
 
+
+    def initialize(discount = 0)
+        @discount = discount
+        self.total = 0  
+        self.food_array = []       
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.last_price = price
+        self.last_quanity = quantity
+        quantity.times do
+            self.food_array.push(title)
+        end
+        total_price = self.total + price * quantity
+        self.total = total_price      
+    end
+
+    def apply_discount
+        if self.discount == 0
+        return "There is no discount to apply."
+        else 
+            sub_price = self.total.to_f * (self.discount.to_f / 100)   
+            discount_price = self.total - sub_price
+        self.total = discount_price
+        return "After the discount, the total comes to $800."
+        end
+    end
+
+    def items
+        self.food_array
+    end
+
+    def void_last_transaction
+       puts self.food_array.pop()
+        new_total = self.total - (self.last_price * self.last_quanity) 
+        self.total = new_total
+    end
+
+end
